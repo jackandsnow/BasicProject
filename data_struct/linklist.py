a8afb0156a7c9f0c6924e4b9ebabf57c98e41bf4
Jack <jyjlwx@outlook.com>
2019-10-08 21:31:58 +0800

    double link list implement

--- data_struct/linklist.py
+++ data_struct/linklist.py
@@ -9,6 +9,9 @@ class SingleLinkNode:
 
 
 class SingleLinkList:
+    """
+    Single Link List implement
+    """
 
     def __init__(self):
         """
@@ -40,12 +43,14 @@ class SingleLinkList:
         if self.isEmpty():
             self.__head = node
         else:
+            length = self.length()
+            index = index if index >= 0 else length + index
             # insert at the first position
             if index == 0:
                 curr = self.__head
                 node.next = curr
                 self.__head = node
-            elif 0 < index < self.length():
+            elif 0 < index < length:
                 count = 1
                 curr = self.__head
                 while curr.next:
@@ -88,7 +93,9 @@ class SingleLinkList:
         if self.isEmpty():
             return None
 
-        if 0 <= index < self.length():
+        length = self.length()
+        index = index if index >= 0 else length + index
+        if 0 <= index < length:
             count = 0
             curr = self.__head
             while curr:
@@ -114,13 +121,13 @@ class SingleLinkList:
             return
 
         curr = self.__head
-        pre = None
+        prev = None
         while curr:
             back = curr.next
-            curr.next = pre
-            pre = curr
+            curr.next = prev
+            prev = curr
             curr = back
-        self.__head = pre
+        self.__head = prev
 
     def length(self):
         """
@@ -160,11 +167,161 @@ class DoubleLinkNode:
 
     def __init__(self, data=None):
         self.data = data
-        self.pre = None
+        self.prev = None
         self.next = None
 
 
 class DoubleLinkList:
+    """
+    Double Link List implement
+    """
 
     def __init__(self):
-        pass
+        """
+        Initialize Double Link List
+        """
+        self.__head = DoubleLinkNode()
+        self.__tail = DoubleLinkNode()
+        self.__head.next = self.__tail
+        self.__tail.prev = self.__head
+
+    def append(self, obj):
+        """
+        append an element at tail
+        :param obj: object for appending
+        """
+        node = DoubleLinkNode(obj)
+        prev_node = self.__tail.prev
+        prev_node.next = node
+        node.prev = prev_node
+        node.next = self.__tail
+        self.__tail.prev = node
+
+    def insert(self, obj, index):
+        """
+        insert an element at specific position
+        :param obj: object for inserting
+        :param index: target position
+        """
+        node = DoubleLinkNode(obj)
+        length = self.length()
+        index = index if index >= 0 else length + index
+        # insert at the first position
+        if index == 0:
+            back = self.__head.next
+            self.__head.next = node
+            node.prev = self.__head
+            node.next = back
+            back.prev = node
+        elif 0 < index < length:
+            count = 1
+            curr = self.__head
+            while curr.next != self.__tail:
+                if count == index:
+                    node.prev = curr
+                    node.next = curr.next
+                    curr.next.prev = node
+                    curr.next = node
+                    break
+                else:
+                    count += 1
+                    curr = curr.next
+        # if index is out of range, insert at last position
+        else:
+            self.append(obj)
+
+    def remove(self, obj):
+        """
+        remove the first element of target object
+        :param obj: target object
+        """
+        if self.isEmpty():
+            pass
+        else:
+            curr = self.__head.next
+            while curr != self.__tail:
+                if curr.data == obj:
+                    prev_node = curr.prev
+                    prev_node.next = curr.next
+                    curr.next.prev = prev_node
+                    break
+                else:
+                    curr = curr.next
+
+    def getValue(self, index):
+        """
+        get value of specific position
+        :param index: specific position
+        :return: value
+        """
+        if self.isEmpty():
+            return None
+
+        length = self.length()
+        index = index if index >= 0 else length + index
+        if 0 <= index < length:
+            count = 0
+            curr = self.__head.next
+            while curr != self.__tail:
+                if count == index:
+                    return curr.data
+                else:
+                    count += 1
+                    curr = curr.next
+        else:
+            return None
+
+    def clear(self):
+        """
+        clear the whole Double Link List
+        """
+        self.__head.next = self.__tail
+        self.__tail.prev = self.__head
+
+    def reverse(self):
+        """
+        reverse the Double Link List
+        """
+        if self.isEmpty() or self.length() == 1:
+            return
+
+        prev = back = self.__tail
+        curr = self.__head.next
+        while curr != self.__tail:
+            back = curr.next
+            curr.next = prev
+            curr.prev = back
+            prev = curr
+            curr = back
+        self.__head.next = prev
+        self.__tail.prev = back.prev
+
+    def length(self):
+        """
+        get the length of Double Link List
+        :return:
+        """
+        count = 0
+        curr = self.__head
+        while curr.next != self.__tail:
+            count += 1
+            curr = curr.next
+        return count
+
+    def isEmpty(self):
+        """
+        judge the Double Link List is empty or not
+        :return: bool value
+        """
+        return self.__head.next == self.__tail
+
+    def print_all(self):
+        """
+        print all elements of Double Link List
+        """
+        element = []
+        curr = self.__head.next
+        while curr != self.__tail:
+            element.append(curr.data)
+            curr = curr.next
+        print(element)
