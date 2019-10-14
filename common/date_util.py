d8e4cdff8e50becbda08a11c5e1f7ba5b5a930e8
Jack <jyjlwx@outlook.com>
2019-10-14 19:20:24 +0800

    add between days function

--- common/date_util.py
+++ common/date_util.py
@@ -27,3 +27,23 @@ def string_to_date(date_str, fmt=None, n=0):
     if not fmt:
         fmt = format_list[n]
     return datetime.datetime.strptime(date_str, fmt)
+
+
+def get_between_days(f_date, t_date):
+    """
+    get between days from f_date to t_date
+    :param f_date: datetime or str, like '%Y-%m-%d'
+    :param t_date: datetime or str, like '%Y-%m-%d'
+    :return: everyday str list
+    """
+    days_list = []
+    if isinstance(f_date, str):
+        f_date = datetime.datetime.strptime(f_date, '%Y-%m-%d')
+    if isinstance(t_date, str):
+        t_date = datetime.datetime.strptime(t_date, '%Y-%m-%d')
+
+    while f_date <= t_date:
+        day_str = datetime.datetime.strftime(f_date, '%Y%m%d')
+        days_list.append(day_str)
+        f_date += datetime.timedelta(days=1)
+    return days_list
