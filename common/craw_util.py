5af4548dad01cf124b832fb7840bdb5fc0dfd850
Jack <jyjlwx@outlook.com>
2019-11-12 22:20:24 +0800

    deal with IndexError

--- common/craw_util.py
+++ common/craw_util.py
@@ -100,13 +100,17 @@ def download_paper(doi, filename=None):
     :param filename: '/path/filename.pdf'
     :return: None
     """
-    sci_hub = 'https://sci-hub.tw/'
-    html_text = get_html_text(sci_hub + doi)
-    if html_text:
-        html = etree.HTML(html_text)
-        url = html.xpath('//div[@id="article"]/iframe/@src')[0]
-        url = url.replace('#view=FitH', '?download=true')
-        filename = filename if filename else url.split('?')[0].split('/')[-1]
-        flag = download_file('https:' + url, filename)
-        if not flag:
-            print('Download Paper DOI[' + doi + '] Failed!')
+    try:
+        sci_hub = 'https://sci-hub.tw/'
+        html_text = get_html_text(sci_hub + doi)
+        if html_text:
+            html = etree.HTML(html_text)
+            url = html.xpath('//div[@id="article"]/iframe/@src')[0]
+            url = url.replace('#view=FitH', '?download=true')
+            filename = filename if filename else url.split('?')[0].split('/')[-1]
+            flag = download_file('https:' + url, filename)
+            if not flag:
+                print('Download Paper DOI[' + doi + '] Failed!')
+    # Verification code skipped
+    except IndexError:
+        print(doi + ': Error with getting download url!')
